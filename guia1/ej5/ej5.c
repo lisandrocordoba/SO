258464a7cab2cc8ce2ea579d1d7eaaaa159730ad
lisandrocordoba <lisandrocordoba11@example.com>
2025-09-07 00:44:09 -0300

    pruebo el gitignore

--- guia1/ej5/ej5.c
+++ guia1/ej5/ej5.c
@@ -4,36 +4,66 @@
 #include <sys/wait.h>
 
 void bart(){
-    printf("Bart\n");
+    printf("Soy Bart, mi pid es: %d y el de mi papa es: %d\n", getpid(), getppid());
+    exit(EXIT_SUCCESS);
 }
 
 void lisa(){
-    printf("Lisa\n");
+    printf("Soy Lisa, mi pid es: %d y el de mi papa es: %d\n", getpid(), getppid());
+    exit(EXIT_SUCCESS);
 }
 
 void maggie(){
-    printf("Maggie\n");
+    printf("Soy Maggie, mi pid es: %d y el de mi papa es: %d\n", getpid(), getppid());
+    exit(EXIT_SUCCESS);
 }
 
 void homero(){
-    if (fork() == 0){
+    printf("Soy Homero, mi pid es: %d y el de mi papa es: %d\n", getpid(), getppid());
+    pid_t pidBart, pidLisa, pidMaggie;
+    pidBart = fork();
+    if (pidBart == 0){
+        // Aca soy Bart
         bart();
-        lisa();
-        maggie();
-    } else {
-        printf("Homero\n");
     }
+    else {
+        // Aca sigo siendo Homero
+        pidLisa = fork();
+        if (pidLisa == 0){
+            // Aca soy Lisa
+            lisa();
+        }
+        else{
+            // Aca sigo siendo Homero
+            pidMaggie = fork();
+            if (pidMaggie == 0){
+                // Aca soy Maggie
+                maggie();
+            }
+        }
+    }
+    // Aca sigo siendo Homero (los demas terminan antes de llegar aca)
+    // Espero a los 3, no se en que orden terminaran
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+    exit(EXIT_SUCCESS);
 }
 
 void abraham(){
+    printf("Soy Abraham, mi pid es: %d y el de mi papa es: %d\n", getpid(), getppid());
     if (fork() == 0){
+        // Aca soy Homero
         homero();
     } else{
-        printf("Abraham\n");
+        // Aca soy Abraham
+        wait(NULL);
+        exit(EXIT_SUCCESS);
     }
 }
 
 int main(){
     abraham();
-    return 0;
+    wait(NULL);
+    exit(EXIT_SUCCESS);
 }
